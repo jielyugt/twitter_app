d36bc4b7eab73f29bcca75ca42ac7d8f3ba90636
jielyugt <jie.lyu@gatech.edu>
2020-03-29 23:08:08 -0400

    server register() finished

--- ttweetser.py
+++ ttweetser.py
@@ -10,77 +10,98 @@ import json
 ### Main Function
 
 def main():
-    
-    # collect arguments
-    server_port = util.server_util.fetch_args(sys.argv)
+	
+	# collect arguments
+	server_port = util.server_util.fetch_args(sys.argv)
 
-    # bind socket to port
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-		server_socket.bind((socket.gethostname(), server_port))
+	# bind socket to port
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		server_socket.bind(('localhost', server_port))
 	except:
 		sys.exit()
-    
-    # prepare data structures
-    socket_users = {}               # dict of socket connection -> user object
+	server_socket.listen(5)
 
-    # select
-    potential_readers = [server_socket]
-    potential_writers = []
-    potential_errors = []
-    message_queues = {}             # dict of socket -> queue of Tweet object
+	# prepare data structures
+	socket_users = {}               # dict of socket connection -> user object
 
-    while True:
-        ready_to_read, ready_to_write, in_error = \
-                select.select(
-                    potential_readers,
-                    potential_writers,
-                    potential_errors)
-        
-        for s in ready_to_read:
+	# select
+	potential_readers = [server_socket]
+	potential_writers = []
+	potential_errors = []
+	message_queues = {}             # dict of socket -> queue of Tweet object
 
-            # if a connection from a new client is requested
-            if s is server_socket:
-                client_connection, client_address = s.accept()
-                potential_readers.append(client_connection)
+	while True:
+		ready_to_read, ready_to_write, in_error = \
+				select.select(
+					potential_readers,
+					potential_writers,
+					potential_errors)
+		
+		for s in ready_to_read:
 
-            # if a already connected client sends a request
-            else:
-                # TODO: read the request, do action and respond
-                str_data = s.recv(2048).decode()
-                dic_data = json.loads(str_data)
-                cmd = dic_data['cmd']
+			# if a connection from a new client is requested
+			if s is server_socket:
+				print('got connection')
+				client_connection, client_address = s.accept()
+				potential_readers.append(client_connection)
 
-                if cmd = 'register':
-                    username = dic_data['username']
-                    # there is no way to pass in username when client calls connect(), so it's a seperate process
-                    util.server_util.register_user(username, s, socket_users)
+			# if a already connected client sends a request
+			else:
+				raw_data = s.recv(2048)
+				dic_data = json.loads(raw_data.decode())
+				cmd = dic_data['cmd']
 
-                if cmd = 'tweet': 
-                    # TODO
-                    message = dic_data['message']
-                    hashtags = dic_data['hashtags']
-                    tag_list = hashtags.strip('#').split('#')
-                    tweet = Tweet(socket_users[s], message, tag_list)
-                    subscribers = util.server_util.tag_to_user(tag_list, list(socket_users.values()))
-                    util.server_util.push_tweet(subscribers, tweet, potential_writers, message_queues)
+				if cmd == 'register':
+					username = dic_data['username']
 
-                elif cmd = 'subscribe':
-                    # TODO
-                #TODO
-        
-        for s in ready_to_write:
-            tweet_queue = message_queues[s]
-            try:
-                tweet_to_send = tweet_queue.pop(0)
-            except:
-                print('Debug: poping from empty list. should not ever trigger this error. if triggered, check logics.')
-            else:
-                s.send(tweet_to_send.push_format())
-            
-            if len(tweet_queue) == 0:
-                potential_writers.remove(s)
-                
+					# register user if no duplicate username
+					status = 'Success'
+					if not util.server_util.register_user(username, s, socket_users):
+						status = 'Error'
+					print(username, status)
+					# send status code
+					util.server_util.send_msg_socket([s],
+													status,
+													potential_writers,
+													message_queues)
+
+				if cmd == 'tweet': 
+					message = dic_data['message']
+					hashtags = dic_data['hashtags']
+
+					# add tweet to user history
+					tag_list = hashtags.strip('#').split('#')
+					tweet = Tweet(socket_users[s], message, tag_list)
+					socket_users[s].tweets_posted.append(tweet)
+
+					# find out subscribers to the hashtags
+					subscribers = util.server_util.tag_to_user(tag_list, list(socket_users.values()))
+					subscribers_sockets = [user.socket for user in subscribers]
+
+					# push tweets
+					util.server_util.send_msg_socket(subscribers_sockets, 
+													tweet.push_format(), 
+													potential_writers, 
+													message_queues)
+
+				elif cmd == 'subscribe':
+					# TODO
+					pass
+				#TODO
+		
+		for s in ready_to_write:
+			message_queue = message_queues[s]
+			try:
+				message_to_send = message_queue.pop(0)
+			except:
+				print('Debug: poping from empty list. should not ever trigger this error. if triggered, check logics.')
+			else:
+				s.send(message_to_send.encode())
+			
+			if len(message_queue) == 0:
+				potential_writers.remove(s)
+				
 
 if __name__ == '__main__':
 	main()
